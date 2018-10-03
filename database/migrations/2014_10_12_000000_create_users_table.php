dd35c0cded218fcb1c123c89a201f7b52307eb36
anonxjc <tjhunecarlo@gmail.com>
2018-10-03 20:13:36 +0800

    updated the table to create admin

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('first_name');
+            $table->string('middle_name');
+            $table->string('last_name');
+            $table->string('username')->unique();
             $table->string('password');
-            $table->rememberToken();
+            $table->rememberToken()->unique();
+            $table->string('api_token')->unique();
             $table->timestamps();
+            $table->unique(['first_name', 'middle_name', 'last_name'], 'fullname');
         });
     }
 
