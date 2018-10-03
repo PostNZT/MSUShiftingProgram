cd91976ab328e09f2c1d53eb2137611db8bc3151
anonxjc <tjhunecarlo@gmail.com>
2018-10-04 01:12:16 +0800

    implemented the custom guards

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,11 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
-class Handler extends ExceptionHandler
+final class Handler extends ExceptionHandler
 {
     /**
      * A list of the exception types that are not reported.
@@ -32,7 +35,7 @@ class Handler extends ExceptionHandler
      * @param  \Exception  $exception
      * @return void
      */
-    public function report(Exception $exception)
+    public function report(Exception $exception) : void
     {
         parent::report($exception);
     }
@@ -47,5 +50,25 @@ class Handler extends ExceptionHandler
     public function render($request, Exception $exception)
     {
         return parent::render($request, $exception);
+    }
+
+     /**
+     * Override to manage the correct login routes based on the customized guards
+     */
+    protected function unauthenticated($request, AuthenticationException $exception) : object
+    {
+        if ($request->expectsJson()) {
+            return response()->json(['message' => $exception->getMessage()], 401);
+        }
+
+        $guard = array_get($exception->guards(), 0);
+
+        switch ($guard) {
+            case 'admin':
+                $loginRoute = 'admin.login';
+                break;
+        }
+
+        return redirect()->guest(route($loginRoute));
     }
 }
