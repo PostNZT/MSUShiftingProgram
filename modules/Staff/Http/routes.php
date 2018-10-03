198173890a43f8e065c6f92bac648b0af0ddd6af
anonxjc <tjhunecarlo@gmail.com>
2018-10-03 17:09:51 +0800

    initial implementation of Staff Module

---
+++ modules/Staff/Http/routes.php
@@ -0,0 +1,10 @@
+<?php
+
+Route::group(
+	[
+		'middleware' => 'web', 
+		'domain' => 'staff.'.\config('app.domain'), 
+		'namespace' => 'MnkyDevTeam\Staff\Http\Controllers'
+	], function(){
+    Route::get('/', 'StaffController@index');
+});
