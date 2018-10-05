f8af2ebed2d16ee44f997d7cca98be57c3d1e80f
anonxjc <tjhunecarlo@gmail.com>
2018-10-06 03:43:34 +0800

    updated with new features to be used

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -10,6 +10,9 @@
 <link href="{{asset('lib/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
 <!--external css-->
 <link href="{{asset('lib/font-awesome/css/font-awesome.css')}}" rel="stylesheet" />
+<link rel="stylesheet" type="text/css" href="{{asset('css/zabuto_calendar.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('lib/gritter/css/jquery.gritter.css')}}" />
 <!-- Custom styles for this template -->
 <link href="{{asset('css/style.css')}}" rel="stylesheet">
 <link href="{{asset('css/style-responsive.css')}}" rel="stylesheet">
+<script src="{{asset('lib/chart-master/Chart.js')}}"></script>
