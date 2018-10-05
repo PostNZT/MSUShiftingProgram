65c9f5f1e849457e3bfb3fb6bf4b4a5935ce124f
anonxjc <tjhunecarlo@gmail.com>
2018-10-06 03:44:02 +0800

    implemented the master blade file

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -5,12 +5,28 @@
 	<title>@yield('title')</title>
 </head>
 <body>
+	<section id="container">
+		<header class="header black-bg">
+			@include('layout.navbar')
+		</header>
+		<aside>
+			<div id="sidebar" class="nav-collapse ">
+				@include('layout.sidebar')
+			</div>
+		</aside>
+		<section id="main-content">
+	    	<section class="wrapper">
+		        <div class="row">
+					@yield('content')
+		        </div>
+	      	</section>
+		</section>
 
+		<footer class="site-footer">
+		  @include('layout.footer')
+		</footer>
+	</section>
 
-
-	<footer class="site-footer">
-	  @include('layout.footer')
-	</footer>
 	@include('layout.scripts')
 </body>
 </html>
