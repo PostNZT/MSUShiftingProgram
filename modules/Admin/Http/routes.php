3f462bcc94160858fcbfe654edf292a71d3627c4
anonxjc <tjhunecarlo@gmail.com>
2018-10-06 02:04:18 +0800

    implemented the auth routes

--- modules/Admin/Http/routes.php
+++ modules/Admin/Http/routes.php
@@ -6,5 +6,19 @@ Route::group(
 		'domain' => 'admin.'.\config('app.domain'), 
 		'namespace' => 'MnkyDevTeam\Admin\Http\Controllers'
 	], function() {
-    Route::get('/', 'AdminController@index')->name('admin.home');
+		Route::group(['middleware' => 'guest', 'namespace' => "Auth"], function () {
+            Route::view('login', 'admin::login')->name('admin.login');
+            Route::post('login', "LoginController")->name('admin.login.submit');
+        });
+
+        Route::group(['middleware' => 'auth:admin'], function () {
+            Route::get('/', function () {
+                return \redirect(\route('admin.user.dashboard'));
+            })->name('admin.home');
+
+            Route::get('profile', "User\ProfileController")->name('admin.user.dashboard');
+
+            Route::post('logout', "Auth\LogoutController")->name('admin.logout');
+        });
+    
 });
