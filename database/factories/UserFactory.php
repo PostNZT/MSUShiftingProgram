b5e439489922edd2c55e0f5259b8c7a08dc7a99d
anonxjc <tjhunecarlo@gmail.com>
2018-10-06 02:03:31 +0800

    implemented the user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -13,11 +13,14 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(App\Entities\User\User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
+    	'first_name'        => $faker->firstName,
+        'middle_name'       => $faker->lastName,
+        'last_name'         => $faker->lastName,
+        'username'          => $faker->unique()->userName,
+        'password'          => '$2y$12$DnIcVNHOpZP8URBphopyYe08kNZfYnW3/ofDo5b7sydfH4SlwLPi6', // msu.admin
+        'remember_token'    => str_random(100),
+        'api_token'         => str_random(100),
     ];
 });
