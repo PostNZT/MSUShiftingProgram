de6879a618416852ac5c3a1c7f143df7f3e7ed32
anonxjc <tjhunecarlo@gmail.com>
2018-10-06 17:11:27 +0800

    added generate Dummy info for employee

--- tests/Helpers/AdminUserFactoryHelper.php
+++ tests/Helpers/AdminUserFactoryHelper.php
@@ -5,6 +5,7 @@ declare(strict_types = 1);
 namespace Tests\Helpers;
 
 use App\Entities\User\User;
+use App\Entities\Employee\Employee;
 use Illuminate\Support\Facades\Hash;
 
 trait AdminUserFactoryHelper
@@ -17,12 +18,29 @@ trait AdminUserFactoryHelper
         ]);
     }
 
-    public function fakeUser(string $firstName = "Jhune Carlo", string $lastName = "Trogelio", string $middleName = "B") : User
+    public function fakeAdmin(string $firstName = "Jhune Carlo", string $lastName = "Trogelio", string $middleName = "B") : User
     {
         return \factory(User::class)->create([
             'first_name'    => $firstName,
             'middle_name'   => $middleName,
             'last_name'     => $lastName,
         ]);
+    }
+
+    public function generateDummyEmployeeInfo(
+        string $firstName   = "Jhune Carlo",
+        string $middleName  = "B", 
+        string $lastName    = "Trogelio",
+        string $birthdate   = '1978-02-03',
+        int $role_id        = 1
+    ) : array {
+        return \factory(Employee::class)
+            ->make([
+                'first_name'        => $firstName,
+                'middle_name'       => $middleName,
+                'last_name'         => $lastName,
+                'birthdate'         => $birthdate,
+                'role_id'           => $role_id
+            ])->toArray();
     }
 }
