0791570f6d5ca91ad907a4ce1ef5233390ff9685
anonxjc <tjhunecarlo@gmail.com>
2018-10-06 17:09:15 +0800

    updated the master class for the ui

--- modules/Admin/Resources/views/user/dashboard.blade.php
+++ modules/Admin/Resources/views/user/dashboard.blade.php
@@ -1,13 +1,50 @@
 @extends('layout.master')
+@section('header')
+  
+  <!-- Bootstrap core CSS -->
+  <link href="{{asset('lib/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+  <!--external css-->
+  <link href="{{asset('lib/font-awesome/css/font-awesome.css')}}" rel="stylesheet" />
+  <link rel="stylesheet" type="text/css" href="{{asset('css/zabuto_calendar.css')}}">
+  <link rel="stylesheet" type="text/css" href="{{asset('lib/gritter/css/jquery.gritter.css')}}" />
+  <!-- Custom styles for this template -->
+  <link href="{{asset('css/style.css')}}" rel="stylesheet">
+  <link href="{{asset('css/style-responsive.css')}}" rel="stylesheet">
+  <script src="{{asset('lib/chart-master/Chart.js')}}"></script>
+
+@endsection
 @section('title')
 	MSUOVCAA Dashboard
 @endsection
-
+@section('sidebar')
+<ul class="sidebar-menu" id="nav-accordion">
+  <p class="centered"><a href="profile.html"><img src="img/ui-sam.jpg" class="img-circle" width="80"></a></p>
+  <h5 class="centered">Jhune Carlo Trogelio</h5>
+  <li class="mt">
+    <a class="active" href="{{\route('admin.user.dashboard')}}">
+      <i class="fa fa-dashboard"></i>
+      <span>Dashboard</span>
+    </a>
+  </li>
+  <li>
+    <a href="{{\route('admin.employee')}}">
+      <i class="fa fa-group"></i>
+      <span>Employees </span>
+    </a>
+  </li>
+  <li>
+    <a href="">
+      <i class="fa fa-sitemap"></i>
+      <span>Students </span>
+    </a>
+  </li>
+</ul>
+@endsection
 @section('content')
 <div class="col-lg-9 main-chart">
             <!--CUSTOM CHART START -->
             <div class="border-head">
-              <h3>USER VISITS</h3>
+              <h3>Dashboard</h3>
             </div>
             <div class="custom-bar-chart">
               <ul class="y-axis">
@@ -418,4 +455,84 @@
             </div>
             <!-- / calendar -->
           </div>
+@endsection
+
+@section('scripts')
+  <script src="{{asset('lib/jquery/jquery.min.js')}}"></script>
+
+  <script src="{{asset('lib/bootstrap/js/bootstrap.min.js')}}"></script>
+  <script class="include" type="text/javascript" src="{{asset('lib/jquery.dcjqaccordion.2.7.js')}}"></script>
+  <script src="{{asset('lib/jquery.scrollTo.min.js')}}"></script>
+  <script src="{{asset('lib/jquery.nicescroll.js')}}" type="text/javascript"></script>
+  <script src="{{asset('lib/jquery.sparkline.js')}}"></script>
+  <!--common script for all pages-->
+  <script src="{{asset('lib/common-scripts.js')}}"></script>
+  <script type="text/javascript" src="{{asset('lib/gritter/js/jquery.gritter.js')}}"></script>
+  <script type="text/javascript" src="{{asset('lib/gritter-conf.js')}}"></script>
+  <!--script for this page-->
+  <script src="{{asset('lib/sparkline-chart.js')}}"></script>
+  <script src="{{asset('lib/zabuto_calendar.js')}}"></script>
+  <script type="text/javascript">
+    $(document).ready(function() {
+      var unique_id = $.gritter.add({
+        // (string | mandatory) the heading of the notification
+        title: 'Welcome to MSUOVCAA Shifting Program!',
+        // (string | mandatory) the text inside the notification
+        text: 'Hover me to enable the Close Button. You can hide the left sidebar clicking on the button next to the logo.',
+        // (string | optional) the image to display on the left
+        image: 'img/ui-sam.jpg',
+        // (bool | optional) if you want it to fade out on its own or just sit there
+        sticky: false,
+        // (int | optional) the time you want it to be alive for before fading out
+        time: 8000,
+        // (string | optional) the class name you want to apply to that specific message
+        class_name: 'my-sticky-class'
+      });
+
+      return false;
+    });
+  </script>
+  <script type="application/javascript">
+    $(document).ready(function() {
+      $("#date-popover").popover({
+        html: true,
+        trigger: "manual"
+      });
+      $("#date-popover").hide();
+      $("#date-popover").click(function(e) {
+        $(this).hide();
+      });
+
+      $("#my-calendar").zabuto_calendar({
+        action: function() {
+          return myDateFunction(this.id, false);
+        },
+        action_nav: function() {
+          return myNavFunction(this.id);
+        },
+        ajax: {
+          url: "show_data.php?action=1",
+          modal: true
+        },
+        legend: [{
+            type: "text",
+            label: "Special event",
+            badge: "00"
+          },
+          {
+            type: "block",
+            label: "Regular event",
+          }
+        ]
+      });
+    });
+
+    function myNavFunction(id) {
+      $("#date-popover").hide();
+      var nav = $("#" + id).data("navigation");
+      var to = $("#" + id).data("to");
+      console.log('nav ' + nav + ' to: ' + to.month + '/' + to.year);
+    }
+  </script>
+
 @endsection
