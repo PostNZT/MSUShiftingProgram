ead52bc31df81a305b2255eed53e4a1f2d623f1a
anonxjc <tjhunecarlo@gmail.com>
2018-10-08 13:29:18 +0800

    implements the custom couselor auth using is_authorize

--- modules/Counselor/Http/Controllers/Auth/LoginController.php
+++ modules/Counselor/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace MnkyDevTeam\Counselor\Http\Controllers\Auth;
 
+use App\Entities\Employee\Employee;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use MnkyDevTeam\Counselor\Http\Requests\CounselorLoginRequest;
@@ -13,7 +14,14 @@ final class LoginController extends Controller
     public function __invoke(CounselorLoginRequest $request)
     {
         if (Auth::guard('counselor')->attempt(['username' => $request->username, 'password' => $request->password])) {
-            return redirect()->intended(route('counselor.user.dashboard'));
+            
+            $employee = Employee::where('username', $request->username)->first();
+
+            if ($employee->is_authorize) {
+            	return redirect()->intended(route('counselor.user.dashboard'));
+            }
+
+            return redirect()->intended(route('counselor.login'));
         }
 
         return redirect()
