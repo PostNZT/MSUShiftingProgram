f411bfe8d4892d8fd65e0c321a06c3e967b0aad0
Jhune Carlo Trogelio <tjhunecarlo@gmail.com>
2018-12-03 11:09:53 -0800

    added student listing layout

--- modules/Staff/Resources/views/student/listing/page.blade.php
+++ modules/Staff/Resources/views/student/listing/page.blade.php
@@ -70,7 +70,7 @@
   <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
     <section class="panel">
       <div class="panel-body">
-        CARLO
+        @include('staff::student.listing.layouts.student-listing')
       </div>
     </section>
   </div>
