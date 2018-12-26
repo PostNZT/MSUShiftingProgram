4f3cb4b943966bc8bd0fe48645fcd91ff098309c
Jhune Carlo Trogelio <tjhunecarlo@gmail.com>
2018-12-25 18:36:43 -0800

    updated the request details page

--- modules/Staff/Http/Controllers/Student/Request/StudentRequestDetailsPageController.php
+++ modules/Staff/Http/Controllers/Student/Request/StudentRequestDetailsPageController.php
@@ -5,14 +5,21 @@ declare(strict_types=1);
 namespace MnkyDevTeam\Staff\Http\Controllers\Student\Request;
 
 use Illuminate\View\View;
+use App\Entities\Misc\Gender;
 use App\Entities\Student\Student;
 use Illuminate\Routing\Controller;
+use App\Entities\Misc\CivilStatus;
 
 class StudentRequestDetailsPageController extends Controller
 {
     public function __invoke(Student $student) : View
     {
-        
-        return \view('staff::student.request.details', \compact('student'));
+        $genders = Gender::all();
+        $civil_statuses = CivilStatus::all();
+        return \view('staff::student.request.details', \compact(
+          'student',
+          'genders',
+          'civil_statuses'
+        ));
     }
 }
