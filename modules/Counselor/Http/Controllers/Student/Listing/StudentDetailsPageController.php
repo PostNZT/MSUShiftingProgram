9721536697d603ffb03f32c8c9c26503ee191d98
Jhune Carlo Trogelio <tjhunecarlo@gmail.com>
2018-12-25 18:35:23 -0800

    edited and add some features

--- modules/Counselor/Http/Controllers/Student/Listing/StudentDetailsPageController.php
+++ modules/Counselor/Http/Controllers/Student/Listing/StudentDetailsPageController.php
@@ -5,13 +5,22 @@ declare(strict_types=1);
 namespace MnkyDevTeam\Counselor\Http\Controllers\Student\Listing;
 
 use Illuminate\View\View;
+use App\Entities\Misc\Gender;
 use App\Entities\Student\Student;
 use Illuminate\Routing\Controller;
+use App\Entities\Misc\CivilStatus;
 
 final class StudentDetailsPageController extends Controller
 {
     public function __invoke(Student $student) : View
     {
-        return \view('counselor::student.listing.details', \compact('student'));
+        $genders = Gender::all();
+        $civil_statuses = CivilStatus::all();
+
+        return \view('counselor::student.listing.details', \compact(
+          'student',
+          'genders',
+          'civil_statuses'
+        ));
     }
 }
