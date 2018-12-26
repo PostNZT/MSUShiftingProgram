2e7011b3f9e33bd2d8bf613f355fa6b29394304d
Jhune Carlo Trogelio <tjhunecarlo@gmail.com>
2018-12-25 18:36:19 -0800

    updated the counselor student profile box

--- modules/Counselor/Resources/views/student/listing/layouts/details-student-profile-box.blade.php
+++ modules/Counselor/Resources/views/student/listing/layouts/details-student-profile-box.blade.php
@@ -1,35 +1,38 @@
 <div style="text-align: center;">
-   <div class="servicetitle">
-      <div class="profile-pic">
-        <p><img class="img-circle" src={{$student->picture()}}></p>
-      </div>
-      <h4>{{$student->fullName}}</h4>
-      <hr>
-    </div>
+  <div class="servicetitle">
+     <div class="profile-pic">
+       <p><img class="img-circle" src={{$student->picture()}}></p>
+     </div>
+     <h4>{{$student->fullName}}</h4>
+     <hr>
+   </div>
 </div>
- <hr>
- <div style="text-align: left;">
-    <div class="form-group">
-        <label class="control-label">Employee ID:</label>
-        <label class="control-label"><strong>{{$student->student_id}}</strong></label>
-      <br>
-        <label class="control-label">Contact Number:</label>
-        <label class="control-label"><strong>{{$student->contact_no}}</strong></label>
-      <br>
-        <label class="control-label">Current College:</label>
-        <label class="control-label"><strong>{{$student->old_college()->first()->code}}</strong></label>
-      <br>
-        <label class="control-label">Current College:</label>
-        <label class="control-label"><strong>{{$student->old_course()->first()->name}}</strong></label>
-      <br>
-        <label class="control-label">Shifting College:</label>
-        <label class="control-label"><strong>{{$student->new_college()->first()->code}}</strong></label>
-      <br>
-        <label class="control-label">Current Course:</label>
-        <label class="control-label"><strong>{{$student->new_course()->first()->name}}</strong></label>
-      <br>
-        <label class="control-label">Number Of Times Shifted:</label>
-        <label class="control-label"><strong>{{$student->number_times_shifted}}</strong></label>
-      <br>
-    </div>
- </div>
+<hr>
+<div style="text-align: left;">
+   <div class="form-group">
+       <label class="control-label">Student ID:</label>
+       <label class="control-label"><strong>{{$student->student_id}}</strong></label>
+     <br>
+       <label class="control-label">Year Level:</label>
+       <label class="control-label"><strong>{{$student->year_level}}</strong></label>
+     <br>
+       <label class="control-label">Contact Number:</label>
+       <label class="control-label"><strong>{{$student->contact_no}}</strong></label>
+     <br>
+       <label class="control-label">Current College:</label>
+       <label class="control-label"><strong>{{$student->old_college()->first()->code}}</strong></label>
+     <br>
+       <label class="control-label">Current College:</label>
+       <label class="control-label"><strong>{{$student->old_course()->first()->name}}</strong></label>
+     <br>
+       <label class="control-label">Shifting College:</label>
+       <label class="control-label"><strong>{{$student->new_college()->first()->code}}</strong></label>
+     <br>
+       <label class="control-label">Current Course:</label>
+       <label class="control-label"><strong>{{$student->new_course()->first()->name}}</strong></label>
+     <br>
+       <label class="control-label">Number Of Times Shifted:</label>
+       <label class="control-label"><strong>{{$student->number_times_shifted}}</strong></label>
+     <br>
+   </div>
+</div>
