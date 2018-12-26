9a9ec19a71f379133d38b287351220228346f8b1
Jhune Carlo Trogelio <tjhunecarlo@gmail.com>
2018-12-25 18:38:44 -0800

    updated the details student data box staff

--- modules/Staff/Resources/views/student/request/layout/details-student-data-box.blade.php
+++ modules/Staff/Resources/views/student/request/layout/details-student-data-box.blade.php
@@ -1,21 +1,41 @@
 <div class="panel-heading">
   <ul class="nav nav-tabs nav-justified">
     <li class="active">
-      <a data-toggle="tab" href="#student-info">Student Info</a>
+      <a data-toggle="tab" href="#student-grades">Student Grades</a>
     </li>
     <li>
-      <a data-toggle="tab" href="#student-status">Student Status</a>
+      <a data-toggle="tab" href="#student-status">Student Information</a>
+    </li>
+    <li>
+      <a data-toggle="tab" href="#student-upload">Upload Grades</a>
     </li>
   </ul>
 </div>
 <!-- /panel-heading -->
 <div class="panel-body">
   <div class="tab-content">
-    <div id="student-info" class="tab-pane active">
-
+    <div id="student-grades" class="tab-pane active">
+      <div class="row detailed">
+        <div class=" detailed">
+            <h4 class="mb">Transcript of Records</h4>
+              @include('staff::student.request.layout.tor-listing')
+        </div>
+      </div>
     </div>
     <div id="student-status" class="tab-pane">
-
+      <div class="row detailed">
+        <div class="col-lg-8 col-lg-offset-2 detailed">
+            <h4 class="mb">Personal Information</h4>
+            @include('staff::student.request.layout.personal-info')
+        </div>
+      </div>
+    </div>
+    <div id="student-upload" class="tab-pane">
+      <div class="row detailed">
+        <div class="col-lg-8 col-lg-offset-2 detailed">
+            <h4 class="mb">Personal Information</h4>
+        </div>
+      </div>
     </div>
   </div>
 </div>
