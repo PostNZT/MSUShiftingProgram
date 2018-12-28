a9b3208087a9d56740052a92fdff0e680338edfe
Jhune Carlo Trogelio <tjhunecarlo@gmail.com>
2018-12-28 03:19:32 -0800

    added some features

--- modules/Counselor/Resources/views/student/listing/layouts/details-student-data-box.blade.php
+++ modules/Counselor/Resources/views/student/listing/layouts/details-student-data-box.blade.php
@@ -1,6 +1,9 @@
 <div class="panel-heading">
   <ul class="nav nav-tabs nav-justified">
     <li class="active">
+      <a data-toggle="tab" href="#student-grades">Student Grades</a>
+    </li>
+    <li>
       <a data-toggle="tab" href="#student-info">Student Information</a>
     </li>
     <li>
@@ -11,7 +14,15 @@
 <!-- /panel-heading -->
 <div class="panel-body">
   <div class="tab-content">
-    <div id="student-info" class="tab-pane active">
+    <div id="student-grades" class="tab-pane active">
+      <div class="row detailed">
+        <div class="col-lg-8 col-lg-offset-2 detailed">
+            <h4 class="mb">Transcript of Records</h4>
+            @include('counselor::student.listing.layouts.personal-info')
+        </div>
+      </div>
+    </div>
+    <div id="student-info" class="tab-pane">
       <div class="row detailed">
         <div class="col-lg-8 col-lg-offset-2 detailed">
             <h4 class="mb">Personal Information</h4>
@@ -23,6 +34,7 @@
       <div class="row detailed">
         <div class="col-lg-8 col-lg-offset-2 detailed">
             <h4 class="mb">Shifting Status</h4>
+            @include('counselor::student.listing.layouts.update-status')
         </div>
       </div>
     </div>
