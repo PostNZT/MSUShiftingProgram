ee212889338e0e24f973e131ce49f25e5164e083
anonjc <tjhunecarlo@gmail.com>
2019-01-15 03:48:30 +0700

    finalized the grading view in the counselor

---
+++ modules/Counselor/Http/Controllers/Student/Listing/Api/StudentCounselorGradeListingController.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MnkyDevTeam\Counselor\Http\Controllers\Student\Listing\Api;
+
+use Illuminate\Routing\Controller;
+use App\Traits\DataTable\DataTableTrait;
+use Illuminate\Database\Eloquent\Collection;
+use App\Entities\Student\Student;
+use App\Entities\Student\StudentGradeInformation;
+use MnkyDevTeam\Staff\Http\Resources\Student\StudentGradesCollection;
+use MnkyDevTeam\Staff\Http\Resources\Student\StudentGrades as StudentGradesResource;
+
+final class StudentCounselorGradeListingController extends Controller
+{
+    use DataTableTrait;
+
+    public function listing(Student $student) : Collection
+     {
+
+         $studentGradesCollection = Collection::make(new StudentGradesCollection(new StudentGradesResource(
+             StudentGradeInformation::where(['student_id' => $student->student_id])->get()
+         )));
+
+         return $studentGradesCollection;
+     }
+
+     public function datatable(Student $student)
+     {
+        return $this->makeDataTable($this->listing($student));
+     }
+}
--- modules/Counselor/Http/routes.php
+++ modules/Counselor/Http/routes.php
@@ -29,11 +29,21 @@ Route::group(
 
             Route::group(['prefix' => 'listing', 'namespace' => 'Listing'], function() {
                 Route::get('/', 'StudentListinPageController')->name('counselor.student.listing');
-                Route::get('{student}/details', 'StudentDetailsPageController')->name('counselor.student.listing.details');
-                Route::patch('{student}/details/updateInformation', 'StudentUpdatePersonalInformationController')
-                    ->name('counselor.student.details.update-personal-information');
-                Route::patch('{student}/details/updateStatus', 'StudentUpdateShiftingStatusController')
-                    ->name('counselor.student.details.update-shifting-status');
+
+                Route::group(['prefix' => '{student}/details'], function() {
+                    Route::get('/', 'StudentDetailsPageController')->name('counselor.student.listing.details');
+                    Route::patch('updateInformation', 'StudentUpdatePersonalInformationController')
+                        ->name('counselor.student.details.update-personal-information');
+                    Route::patch('updateStatus', 'StudentUpdateShiftingStatusController')
+                        ->name('counselor.student.details.update-shifting-status');
+
+                    Route::group(['prefix' => 'api', 'namespace' => 'Api'], function() {
+                      Route::get('/listing', 'StudentCounselorGradeListingController@listing')
+                      ->name('counselor.student.details.api.listing');
+                      Route::get('/listing/datatable', 'StudentCounselorGradeListingController@datatable')
+                      ->name('counselor.student.details.api.listing.datatable');
+                    });
+                });
 
                 Route::group(['prefix' => 'api', 'namespace' => 'Api'], function() {
                     Route::get('/listing', 'StudentResourceListingController@listing')
